70355c72032f27c11adc2b4400fd8ef811871529
Pankaj <pankaj@clusterfoundry.in>
2019-11-07 08:54:03 +0530

    Adding POJOS

---
+++ src/main/java/pks/springframework/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package pks.springframework.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/pks/springframework/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package pks.springframework.petclinic.model;
+
+public class Vet extends Person {
+}
